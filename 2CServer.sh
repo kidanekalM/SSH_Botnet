df4bff302722b23f231d78d97bd1465c9296df76
Kidanekal <98110702+kidanekalM@users.noreply.github.com>
2024-04-08 14:10:38 +0300

    Update 2CServer.sh

--- 2CServer.sh
+++ 2CServer.sh
@@ -1,2 +1,6 @@
-# Read from file
-# connect and send 
+# Read from list
+#       e.g list of all hosts [192.168.10.1 . . . ]
+# connect 
+#       e.g telnet 192.168.0.1 
+# send commands  
+#       e.g. ping 192.168.0.1 -c 1000
